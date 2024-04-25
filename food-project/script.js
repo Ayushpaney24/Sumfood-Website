23aa99716e7a235d2776f7a88ffd22f75ba92899
Ayush(Basic) <143201999+Ayushpaney24@users.noreply.github.com>
2024-04-25 14:52:02 +0530

    Add files via upload

---
+++ food-project/script.js
@@ -0,0 +1,26 @@
+gsap.to(".menu",{
+  y:30,
+  duration:1,
+  delay:0.3,
+  stagger:0.5,
+  opacity:1
+}),
+gsap.to(".visual",
+{
+  x:90,
+  duration:1,
+  x:-50
+})
+gsap.to(".text",
+{
+  x:100,
+  duration:1,
+  x:-20
+})
+gsap.to(".btn",
+{
+  x:100,
+  delay:2,
+  duration:1,
+  x:200
+})
